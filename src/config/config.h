6fe03c6118521587697a471cade369b52f1a90fb
Kyriakos <kyriakos.bakirlis@gmail.com>
2025-08-14 20:02:06 +0200

    Update valve pin comment and increase main tank fill timeout

--- src/config/config.h
+++ src/config/config.h
@@ -3,7 +3,7 @@
 #define NUM_PUMPS 6
 #define NUM_FERTILIZERS 4
 #define MAIN_TANK_CHANNEL 5
-#define VALVE_PIN 25 // GPIO pin for solenoid valve
+#define VALVE_PIN 25 // GPIO pin for solenoid valve - D32
 #define AUX_PUMP_CHANNEL 6 // Peristaltic pump for auxiliary tank
 #define LIQUID_SENSOR_PIN 32 // Capacitive liquid sensor pin
 #define MAIN_TANK_FILL_TIMEOUT_MS 66000 // Default: 1 minute, can be changed
