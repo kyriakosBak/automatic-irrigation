a4c298eaef6101b4224d077df86d7430835d6a7e
Kyriakos <kyriakos.bakirlis@gmail.com>
2025-10-14 18:51:44 +0200

    Update MAIN_TANK_FILL_TIMEOUT_MS to 2 minutes for improved watering control

--- src/config/config.h
+++ src/config/config.h
@@ -6,5 +6,5 @@
 #define VALVE_PIN 13 // GPIO pin for solenoid valve
 #define HUMIDIFIER_PUMP_CHANNEL 7 // Peristaltic pump for humidifier tank
 #define LIQUID_SENSOR_PIN 32 // Capacitive liquid sensor pin
-#define MAIN_TANK_FILL_TIMEOUT_MS 60000 // Default: 1 minute, can be changed
+#define MAIN_TANK_FILL_TIMEOUT_MS 120000 // Default: 2 minutes, can be changed
 #define MAX_WATERING_TIME_MS 300000 // Maximum watering time in milliseconds (5 minutes default)
